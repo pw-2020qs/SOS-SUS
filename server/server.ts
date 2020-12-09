6b3561a9e0e1db41e2449df5850d8ae671f8a546
sergioppires <sergioppires@live.com>
2020-12-09 17:52:54 -0300

    Implementação da consulta ao DataSUs

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,16 @@
 import express = require('express');
 import { hospitalList } from './models/Hospital';
 import mongoose = require('mongoose');
-import request = require('request');
-//const request = require('request');
+global.fetch = require("node-fetch");
+import retornaListaCNES from './controller/DataSUSService'
+
 
 const app: express.Application = express();
 
 
 const uri: string = "mongodb://sossusadmin:rYHr9CR5pmLk!EP@dbh11.mlab.com:27117/sossus";
 
-mongoose.connect(uri,{useNewUrlParser: true }, (err: any) => {
+mongoose.connect(uri, { useNewUrlParser: true }, (err: any) => {
     if (err) {
         console.log(err.message);
     } else {
@@ -21,8 +22,8 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/teste2', function (req, res) {
-    res.send(retornaListaCNES(["2077418", "2077531"]));
+app.get('/teste2', async function (req, res) {
+    res.send(await retornaListaCNES(["2077418", "2077531"]));
 });
 
 app.get('/teste', function (req, res) {
@@ -36,32 +37,29 @@ app.listen(3000, function () {
     console.log('App is listening on port 3000!');
 });
 
-function retornaListaCNES(cnesList: String[]){
-    const options = {
-        url: 'https://elastic-leitos.saude.gov.br/leito_ocupacao/_search?size=150',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic dXNlci1hcGktbGVpdG9zOmFRYkxMM1pTdGFUcjM4dGo='
-        },
-        json: true,
-        body: {
-                "query": {
-                  "terms": {
-                    "cnes": cnesList
-                  }
-                }
+async function retornaListaCNESDecoy(cnesList: String[]) {
+    const url = 'https://elastic-leitos.saude.gov.br/leito_ocupacao/_search?size=150'
+    const bodyRequest = JSON.stringify({
+        "query": {
+            "terms": {
+                "cnes": cnesList
             }
-      }
-
-    request.post(options, (err: any, res: { statusCode: any; }, body: string) => {
-        if (err) {
-            console.log(err);
         }
-        console.log(JSON.stringify(body))
-        return body;
-    });
+    })
 
-    return JSON.stringify(request);
+    const response = await fetch(url,{
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic dXNlci1hcGktbGVpdG9zOmFRYkxMM1pTdGFUcjM4dGo='
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: bodyRequest
+    })    
+    return response.json();
 }
 
