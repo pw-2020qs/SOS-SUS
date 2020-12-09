9e4edeae0a61c3221ca1033590e16b943904d76e
Luiz <lufelipe111@gmail.com>
2020-12-09 14:55:33 -0300

    getting hospitals from back-end

--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Marker, Popup} from 'react-leaflet'
+import { Marker, Popup } from 'react-leaflet'
 import MapComponent from '../components/MapComponent';
 import HospitalCardComponent from '../components/HospitalCardComponent'
 import Sidebar from '../components/Sidebar';
@@ -7,10 +7,23 @@ import smallLogo from '../images/logo_SOS_SUS_mini.png';
 import '../styles/pages/MapPage.css'
 import { Link } from 'react-router-dom';
 import BtnComponent from '../components/BtnComponent';
+import api from '../utils/api';
+
+interface hospital {
+  name: string,
+  estado: string,
+  rua: string,
+  latitude: number,
+  longitude: number,
+}
 
 const MapPage: React.FC = () => {
-  useEffect(() => {
+  let hosps: Array<hospital> = []
 
+  useEffect(() => {
+    api.get('02611001').then(res => {
+      hosps = res.data
+    })
   }, [])
 
   const [toggle, setToggle] = useState('open')
@@ -19,25 +32,37 @@ const MapPage: React.FC = () => {
     if (toggleStatus === 'open') return 'close'
     return 'open'
   }
-  
+
   const handleClick = () => {
     setToggle(toggleMenu(toggle))
   }
 
   const hospitals = () => {
-    
+    hosps.map(hosp => {
+      return (
+        <HospitalCardComponent
+          name={hosp.name}
+          estado={hosp.estado}
+          rua={hosp.rua}
+          latitude={hosp.latitude}
+          longitude={hosp.longitude}
+          link1='Como chegar?'
+          link2='Copiar endereço'
+        />
+      )
+    })
   }
 
   return (
     <div id="page-map">
       <div id="main">
-        <Sidebar toggle={ toggle }>
+        <Sidebar toggle={toggle}>
           <div className="alignIconRight">
             <img className="iconFormat" id="small-logo" src={smallLogo} alt="small_logo" />
           </div>
           <div className="cards">
-            
-            <HospitalCardComponent
+            {hospitals}
+            {/* <HospitalCardComponent
               name='Hospital Santa Terezinha'
               estado='São Paulo'
               rua='Avenida A - Bairro Y, numero 27'
@@ -71,7 +96,7 @@ const MapPage: React.FC = () => {
               rua='Avenica C - Bairro Z, Numero 99'
               link1='Como chegar?'
               link2='Copiar endereço'
-            />
+            /> */}
           </div>
           <div className="gridBotao">
             <div className="botaoGrande">
