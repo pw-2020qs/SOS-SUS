34400a024c16aa2dfcacf448976cff578bfafcfe
Luiz <lufelipe111@gmail.com>
2020-12-11 03:13:22 -0300

    CEP to coords and map recenter

--- client/src/pages/Address.tsx
+++ client/src/pages/Address.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Marker, Popup } from 'react-leaflet';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import MapComponent from '../components/MapComponent';
 import AddressFormComponent from '../components/FormularioEnderecoComponent';
@@ -8,8 +8,60 @@ import BtnComponent from '../components/BtnComponent';
 import smallLogo from '../images/logo_SOS_SUS_mini.png';
 
 import '../styles/pages/Address.css'
+import { google } from '../services/api';
+
+interface CEPResponse {
+  geometry: {
+    location: {
+      lat: number;
+      lng: number
+    }
+  };
+
+}
 
 const Address: React.FC = () => {
+  const [lat, setLat] = useState(-23.533773)
+  const [long, setLong] = useState(-46.625290)
+
+  const [redirect, setRedirect] = useState(false)
+
+  const [CEP, setCEP] = useState("")
+  const history = useHistory()
+
+  useEffect(() => {
+    if (lat !== 0 && long !== 0 && redirect) {
+      history.push(`/map?lat=${lat}&long=${long}`)
+    }
+  }, [redirect])
+
+  const getLatLong = React.useCallback(async () => {
+    const res = await google(CEP)
+
+    const data = res.data
+
+    const results: Array<CEPResponse> = data.results
+
+    return results[0].geometry.location;
+  }, [CEP])
+
+  const onBlur = async () => {
+    const { lat: latitude, lng: longitude } = await getLatLong();
+
+    setLat(latitude)
+    setLong(longitude)
+  }
+
+  const clickBtn = async () => {
+    setRedirect(true)
+
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCEP(event.target.value)
+    console.log(event.target.value)
+  }
+
   return (
     <div id="page-map">
       <div id="main">
@@ -17,23 +69,22 @@ const Address: React.FC = () => {
           <div className="alignIconRight">
             <img className="iconFormat" id="small-logo" src={smallLogo} alt="small_logo" />
           </div>
-          <AddressFormComponent/>
+
+          <AddressFormComponent onChange={handleChange} onSubmit={clickBtn} onBlur={onBlur} />
+
           <div className="gridBtn grid-template-area">
             <div className="btnLarge">
-              <Link to='/map'>
-                <BtnComponent>Procurar hospital</BtnComponent>
-              </Link>
+              <BtnComponent onClick={clickBtn}>Procurar hospital</BtnComponent>
             </div>
+
             <div className="btnSmall">
-              <Link to='/'>
-                <BtnComponent><i className="fas fa-arrow-left"></i></BtnComponent>
-              </Link>
+              <BtnComponent onClick={history.goBack}><i className="fas fa-arrow-left"></i></BtnComponent>
             </div>
           </div>
         </Sidebar>
-        <MapComponent latitude={-23.468226} longitude={-46.637794} zoom={16}>
-          <Marker key={"a123"} position={[-23.468226, -46.637794]}>
-          <Popup closeButton={true} minWidth={240} maxWidth={240} className="map-popup">
+        <MapComponent latitude={lat} longitude={long} zoom={12}>
+          <Marker key={"a123"} position={[lat, long]}>
+            <Popup closeButton={true} minWidth={240} maxWidth={240} className="map-popup">
               nome teste
           </Popup>
           </Marker>
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,4 +4,6 @@ const api = axios.create({
   baseURL: 'http://localhost:3333',
 })
 
+export const google = (CEP: string) => axios.get(`https://maps.google.com/maps/api/geocode/json?key=${process.env.REACT_APP_API_KEY}&components=country:BR|postal_code:${CEP}`)
+
 export default api;
