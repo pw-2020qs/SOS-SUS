be7e350c4d4dfb757938c128f80e0b63c898a015
MFrizo <matheus.fonseca@quintoandar.com.br>
2020-12-13 15:49:02 -0300

    Move Hospital Lists to Constants

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,10 @@
+import cors = require('cors');
 import express = require('express');
-import { hospitalList } from './models/Hospital';
 import mongoose = require('mongoose');
 global.fetch = require("node-fetch");
+import { hospitalList } from './constants/hospitalList';
 import retornaListaCNES from './controller/DataSUSService'
-import cors = require('cors');
-<<<<<<< HEAD
-import { CacheSchema } from './controller/Cache'
 const cacheModel = require('./controller/Cache')
-
-=======
->>>>>>> 2b2eb73... CEP to coords and map recenter
 
 const app = express();
 
@@ -18,56 +13,55 @@ const uri: string = "mongodb://sossusadmin:rYHr9CR5pmLk!EP@dbh11.mlab.com:27117/
 app.use(cors())
 
 mongoose.connect(uri,{useNewUrlParser: true }, (err: any) => {
-    if (err) {
-        console.log(err.message);
-    } else {
-        console.log(`Connecting to MONGO`);
-    }
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(`Connecting to MONGO`);
+  }
 });
 
 app.get('/chamada', (req, res) => {
-    console.log(`lat: ${req.query.lat} long: ${req.query.long}`)
-    res.send(hospitalList)
+  console.log(`lat: ${req.query.lat} long: ${req.query.long}`)
+  res.send(hospitalList)
 })
 
 app.get('/testeBancoConsulta', async (req, res) => {
-        const resultado = await cacheModel.find({idLocalizacao: req.query.id})
+  const resultado = await cacheModel.find({idLocalizacao: req.query.id})
 
-    try{
-        res.send(resultado)
-    } catch (err) {
-        res.status(500).send(err);
-    }    
+  try{
+    res.send(resultado)
+  } catch (err) {
+    res.status(500).send(err);
+  }    
 })
 
 app.get('/testeBancoGrava', async (req, res) => {
     const payloadGrava = new cacheModel({idLocalizacao: req.query.id, hospitalData: hospitalList})
 
     try{
-        await payloadGrava.save();
-        res.send(payloadGrava);
+      await payloadGrava.save();
+      res.send(payloadGrava);
     } catch (err) {
-        res.status(500).send(err);
+      res.status(500).send(err);
     }    
 })
 
 app.get('/', function (req, res: any) {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.get('/teste2', async function (req, res) {
-    res.send(await retornaListaCNES(["2077418", "2077531"]));
+  res.send(await retornaListaCNES(["2077418", "2077531"]));
 });
 
 app.get('/teste', function (req, res) {
-    if (req.query.cep == "123") {
-        res.send("Erro")
-    }
-    console.log(hospitalList)
-    res.send(hospitalList);
+  if (req.query.cep == "123") {
+    res.send("Erro")
+  }
+  console.log(hospitalList)
+  res.send(hospitalList);
 });
 
 app.listen(3333, function () {
-    console.log('App is listening on port 3333!');
+  console.log('App is listening on port 3333!');
 });
-
